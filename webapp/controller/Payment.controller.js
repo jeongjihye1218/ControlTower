c7b8a56b6934205cb33a7f60f6d0a367c28afbda
sjkim <sejin2740@noaats.com>
2024-08-27 14:45:40 +0900

    controlltower_ksj

--- webapp/controller/Payment.controller.js
+++ webapp/controller/Payment.controller.js
@@ -1,16 +1,53 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/UIComponent"
+], function(Controller, JSONModel, UIComponent) {
 	"use strict";
 
 	return Controller.extend("ControlTower.controller.Payment", {
 
 		onInit: function() {
 			var oModel = new sap.ui.model.json.JSONModel({
-				previewText: ""
+				previewText: "",
+				selectedItem: {}
 			});
 			this.getView().setModel(oModel);
 
+			var oRouter = UIComponent.getRouterFor(this);
+			oRouter.getRoute("Payment").attachPatternMatched(this._onObjectMatched, this);
+
+		},
+
+		_onObjectMatched: function(oEvent) {
+
+			// URL 파라미터에서 데이터를 가져옵니다.
+			var sData = oEvent.getParameter("arguments").data;
+			console.log("sejin:", sData);
+			try {
+				// 디코딩 후 JSON 파싱
+				var oSelectedItem = JSON.parse(decodeURIComponent(sData));
+				
+				// 객체를 JSON 문자열로 변환하여 모델에 설정
+				var sPreviewText = JSON.stringify(oSelectedItem, null, 2); // JSON 문자열로 변환 (보기 쉽게 들여쓰기 추가)
+				console.log("받은 데이터:", sPreviewText);
+				// 필요한 정보만 추출하여 초기 문자열 생성
+				var sPreviewText = "포지션일자: " + (oSelectedItem.Bdate || "N/A") + "\n";
+				sPreviewText += "상품구분: " + (oSelectedItem.ProductGbnTxt || "N/A") + "\n";
+				sPreviewText += "운용부서: " + (oSelectedItem.DepartNm || "N/A") + "\n";
+				sPreviewText += "종목명: " + (oSelectedItem.Bdate || "N/A") + "\n";
+				sPreviewText += "거래번호: " + (oSelectedItem.DealNumber || "N/A") + "\n";
+				sPreviewText += "수량: " + (oSelectedItem.LocalAmt || "N/A") + "\n";
+				sPreviewText += "종목명: " + (oSelectedItem.RanlTxt || "N/A");
+
+				// 데이터를 모델에 설정합니다.
+				this.getView().getModel().setProperty("/previewData", sPreviewText);
+
+				// 미리보기 탭 업데이트
+				this.updatePreview();
+			} catch (e) {
+				console.error("데이터 파싱 오류:", e);
+			}
 		},
 
 		onTabSelect: function(oEvent) {
@@ -21,9 +58,24 @@ sap.ui.define([
 		},
 
 		updatePreview: function() {
-
+			
+			//var sCurrentPreviewText = this.getView().getModel().getProperty("/previewData");
 			var sRemark = this.getView().byId("idRemark").getValue();
-			this.getView().getModel().setProperty("/previewText", sRemark);
+			this.getView().getModel().setProperty("/information", sRemark);
+			
+			// if(sRemark){
+			// 	sRemark = "기본정보 : " + sRemark;
+			// 	sCurrentPreviewText += sRemark;
+			// }else
+			
+			// if (sCurrentPreviewText) {
+			// 	var sRemark = this.getView().byId("idRemark").getValue();
+
+			// 	sRemark = "결재리턴Key : " + sRemark;
+			// 	sRemark = sCurrentPreviewText + sRemark;
+			// } else {
+			// 	this.getView().getModel().setProperty("/previewText", sRemark);
+			// }
 		},
 
 		onPressButton1: function() {
@@ -36,7 +88,7 @@ sap.ui.define([
 			// var sPath = "/ZApprid_numcSet"; // 엔티티 세트의 경로
 			var oModel = this.getOwnerComponent().getModel("ZPJ_PAYMENT_SRV");
 			var sPath = "/ZApprid_numcSet";
-	
+
 			// OData 모델을 통해 데이터를 읽어옴
 			oModel.read(sPath, {
 				success: function(oData, response) {
@@ -46,12 +98,12 @@ sap.ui.define([
 					// 예: 데이터를 로컬 모델에 저장하거나 UI에 바인딩
 					var oLocalModel = new sap.ui.model.json.JSONModel(oData.results);
 					this.getView().setModel(oLocalModel, "localModel");
-					
-					oData.results.forEach(function(item){
+
+					oData.results.forEach(function(item) {
 						this.getView().byId("idApprovalKey").setValue(item.AppridNumc);
 					}.bind(this));
-					
-				    // console.log("TEST", oLocalModel.getData("AppridNumc"););
+
+					// console.log("TEST", oLocalModel.getData("AppridNumc"););
 				}.bind(this), // this를 유지하기 위해 bind 사용
 
 				error: function(oError) {
--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -3,135 +3,184 @@ sap.ui.define([
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/comp/valuehelpdialog/ValueHelpDialog",
 	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
-], function(Controller,JSONModel,ValueHelpDialog,Filter,FilterOperator) {
+	"sap/ui/model/FilterOperator",
+	"sap/ui/core/UIComponent"
+], function(Controller, JSONModel, ValueHelpDialog, Filter, FilterOperator, UIComponent) {
 	"use strict";
-	
+
 	var MainoModel;
-	
+
 	return Controller.extend("ControlTower.controller.View1", {
-		
-		onInit: function () {
-			
-    	var sUrl = "/sap/opu/odata/sap/ZPJ_PAYMENT_SRV/";
-    	var MainoModel = new sap.ui.model.odata.ODataModel(sUrl, true);
-    	this.getView().setModel(MainoModel);
-	
+
+		onInit: function() {
+
+			var sUrl = "/sap/opu/odata/sap/ZPJ_PAYMENT_SRV/";
+			var MainoModel = new sap.ui.model.odata.ODataModel(sUrl, true);
+			this.getView().setModel(MainoModel);
+
 		},
 
-        
-
-		
-		onVHKontrh: function(oEvent){
+		onVHKontrh: function(oEvent) {
 			console.log("aaaaaa");
-			
-	  var oInput = oEvent.getSource().getValue();
-      var oValueHelpDialog = new ValueHelpDialog({
-        title: "Select Value",
-        supportMultiselect: true,
-        supportRangesOnly: false,
-        ok: function (oEvent) {
-          var oSelectedItem = oEvent.getParameter("tokens")[0];
-          if (oSelectedItem) {
-            var sSelectedKey = oSelectedItem.getKey();
-            var sSelectedText = oSelectedItem.getText();
-            // 선택된 값을 Input 필드에 설정
-            oInput.setValue(sSelectedText);
-          }
-          oValueHelpDialog.close();
-        },
-        cancel: function () {
-          oValueHelpDialog.close();
-        }
-      });
 
-      // 데이터 모델 설정
-      //var oModel = new JSONModel({
-      //  EntitySet: [
-      //    { ID: "1", Name: "Item 1" },
-      //    { ID: "2", Name: "Item 2" }
-      //  ]
-      //});
-      
-      //oValueHelpDialog.setModel(oModel);
-      var oModel = this.getView().getModel();
-      oValueHelpDialog.setModel(oModel);
-	
-      // 테이블 설정 및 데이터 바인딩
-      var oTable = oValueHelpDialog.getTable();
-      if (oTable.bindRows) {
-        oTable.bindAggregation("rows", "/TpmShplSecAccGrpSet", new sap.ui.table.Row({
-          cells: [
-            new sap.m.Text({ text: "{CompanyCode}" }),
-            new sap.m.Text({ text: "{SecAcctGrp}" })
-          ]
-        }));
-      }
-      
-	console.log(oTable);
-      // Value Help Dialog 열기
-      oValueHelpDialog.open(oInput);		
-		
+			var oInput = oEvent.getSource().getValue();
+			var oValueHelpDialog = new ValueHelpDialog({
+				title: "Select Value",
+				supportMultiselect: true,
+				supportRangesOnly: false,
+				ok: function(oEvent) {
+					var oSelectedItem = oEvent.getParameter("tokens")[0];
+					if (oSelectedItem) {
+						var sSelectedKey = oSelectedItem.getKey();
+						var sSelectedText = oSelectedItem.getText();
+						// 선택된 값을 Input 필드에 설정
+						oInput.setValue(sSelectedText);
+					}
+					oValueHelpDialog.close();
+				},
+				cancel: function() {
+					oValueHelpDialog.close();
+				}
+			});
+
+			// 데이터 모델 설정
+			//var oModel = new JSONModel({
+			//  EntitySet: [
+			//    { ID: "1", Name: "Item 1" },
+			//    { ID: "2", Name: "Item 2" }
+			//  ]
+			//});
+
+			//oValueHelpDialog.setModel(oModel);
+			var oModel = this.getView().getModel();
+			oValueHelpDialog.setModel(oModel);
+
+			// 테이블 설정 및 데이터 바인딩
+			var oTable = oValueHelpDialog.getTable();
+			if (oTable.bindRows) {
+				oTable.bindAggregation("rows", "/TpmShplSecAccGrpSet", new sap.ui.table.Row({
+					cells: [
+						new sap.m.Text({
+							text: "{CompanyCode}"
+						}),
+						new sap.m.Text({
+							text: "{SecAcctGrp}"
+						})
+					]
+				}));
+			}
+
+			console.log(oTable);
+			// Value Help Dialog 열기
+			oValueHelpDialog.open(oInput);
+
 		},
-		
+
 		// onSerch: function(oEvent){
 		// 	// alert("실행");
 		// },
-		
-		onComboBoxChange: function(oEvent){
+
+		onComboBoxChange: function(oEvent) {
 			var oSmartFilterBar = this.getView().byId("smartFilterBar");
-    		var oComboBox = oEvent.getSource();
-    		
-    		var selectedKey = oComboBox.getSelectedKeys();
+			var oComboBox = oEvent.getSource();
+
+			var selectedKey = oComboBox.getSelectedKeys();
 			// console.log(selectedKey);
-    		
-    		// 필터 데이터 업데이트
-    		// var oFilterData = oSmartFilterBar.getFilterData();
-    		var oFilterData = oSmartFilterBar.getFilterData() || {};
-    		// oFilterData.ProductGbn = selectedKey; // 필터 데이터에 선택된 키를 설정
-    		
-    		if(oComboBox.getId().indexOf("AccountGb") != -1){
-    			oFilterData.AccountGb = selectedKey; // 필터 데이터에 선택된 키를 설정
-    		}
-    		else if(oComboBox.getId().indexOf("ProductGbn") != -1){
-    			oFilterData.ProductGbn = selectedKey; // 필터 데이터에 선택된 키를 설정
-    		}
-    		else if(oComboBox.getId().indexOf("DomesticGb") != -1){
-    			oFilterData.DomesticGb = selectedKey; // 필터 데이터에 선택된 키를 설정
-    		}
-    		
-    		// SmartFilterBar에 필터 데이터 설정
-    		oSmartFilterBar.setFilterData(oFilterData,true); //true 설정시 강제 업데이트
+
+			// 필터 데이터 업데이트
+			// var oFilterData = oSmartFilterBar.getFilterData();
+			var oFilterData = oSmartFilterBar.getFilterData() || {};
+			// oFilterData.ProductGbn = selectedKey; // 필터 데이터에 선택된 키를 설정
+
+			if (oComboBox.getId().indexOf("AccountGb") != -1) {
+				oFilterData.AccountGb = selectedKey; // 필터 데이터에 선택된 키를 설정
+			} else if (oComboBox.getId().indexOf("ProductGbn") != -1) {
+				oFilterData.ProductGbn = selectedKey; // 필터 데이터에 선택된 키를 설정
+			} else if (oComboBox.getId().indexOf("DomesticGb") != -1) {
+				oFilterData.DomesticGb = selectedKey; // 필터 데이터에 선택된 키를 설정
+			}
+
+			// SmartFilterBar에 필터 데이터 설정
+			oSmartFilterBar.setFilterData(oFilterData, true); //true 설정시 강제 업데이트
 
 		},
-		
-		onComboTest: function(oEvent){
-			
-		},
-		
-		onBeforeRebindTable: function (oEvent) {
-            var oSmartFilterBar = this.byId("smartFilterBar");
-            // var oDateValue = this.byId("Bdate").getDateValue(); // DatePicker의 값을 가져옴
-            var oDateValue = this.byId("Bdate").getValue();
-            
-            if (oDateValue) {
- 
-        		var oBindingParams = oEvent.getParameter("bindingParams");
-        		var oFilter = new Filter("Bdate", FilterOperator.EQ, oDateValue);
-        		oBindingParams.filters.push(oFilter);
-            }
-        },
-		
-		onAddButton: function(oEvent){
-			    var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 
-    		    if (oRouter) {
-        			oRouter.navTo("Payment");
-    			} else {
-        			console.error("Router not found.");
-    			}
-    			
+		onComboTest: function(oEvent) {
+
+		},
+
+		onAfterRendering: function() {
+			var oSmartTable = this.getView().byId("smartTable01");
+			var oTable = oSmartTable.getTable(); // SmartTable 내부의 Table 객체 가져오기
+
+			if (oTable) {
+				// sap.ui.table.Table에서 단일 선택 모드 설정
+				oTable.setMode("MultiSelect");
+				// 이벤트 핸들러가 중복 등록되지 않도록 기존 핸들러를 먼저 제거한 후 다시 추가
+				oTable.detachSelectionChange(this.onSelectionChange, this);
+				oTable.attachSelectionChange(this.onSelectionChange, this);
+			}
+		},
+
+		onSelectionChange: function(oEvent) {
+
+			var oTable = oEvent.getSource(); // 이벤트 소스에서 테이블을 가져옴
+			var aSelectedItems = oTable.getSelectedItems(); // 선택된 항목 배열 가져오기
+
+			if (aSelectedItems.length > 0) {
+				// 첫 번째 선택된 항목의 바인딩 컨텍스트 가져오기
+				var oSelectedItemContext = aSelectedItems[0].getBindingContext();
+				console.log("oSelectedItemContext: ", oSelectedItemContext);
+
+				if (oSelectedItemContext) {
+					this._selectedItem = oSelectedItemContext.getObject(); // 바인딩된 데이터 객체 가져오기
+				} else {
+					console.warn("선택된 항목의 바인딩 컨텍스트를 가져올 수 없습니다.");
+					this._selectedItem = null;
+				}
+			} else {
+				this._selectedItem = null; // 선택된 항목이 없으면 null로 설정
+				console.log("선택된 항목이 없습니다.");
+			}
+		},
+
+		onBeforeRebindTable: function(oEvent) {
+			var oSmartFilterBar = this.byId("smartFilterBar");
+			// var oDateValue = this.byId("Bdate").getDateValue(); // DatePicker의 값을 가져옴
+			var oDateValue = this.byId("Bdate").getValue();
+
+			if (oDateValue) {
+
+				var oBindingParams = oEvent.getParameter("bindingParams");
+				var oFilter = new Filter("Bdate", FilterOperator.EQ, oDateValue);
+				oBindingParams.filters.push(oFilter);
+			}
+		},
+
+		onAddButton: function(oEvent) {
+			// var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+
+			// if (oRouter) {
+			// 	oRouter.navTo("Payment");
+			// } else {
+			// 	console.error("Router not found.");
+			// }
+			console.log("this._selectedItem : " + this._selectedItem);
+			if (this._selectedItem) {
+				// 선택된 데이터를 전역 모델에 설정
+
+				var oRouter = UIComponent.getRouterFor(this);
+				// JSON 데이터를 문자열로 변환 후 URI 인코딩
+				var sData = encodeURIComponent(JSON.stringify(this._selectedItem));
+				console.log("kim : " + sData);
+				oRouter.navTo("Payment", {
+					data: sData
+				});
+			} else {
+				sap.m.MessageToast.show("먼저 하나의 행을 선택하세요.");
+			}
+
 		}
-		
+
 	});
 });
